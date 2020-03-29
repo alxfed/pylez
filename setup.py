066e9af2ef094139729f792092ace8b1347af510
alxfed <alxfed@gmail.com>
2020-03-29 10:00:00 -0500

    initial

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="pylez",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Alex Fedotov",
+    author_email="alex.fedotov@aol.com",
+    description="Rules Engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/alxfed/pylez",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
