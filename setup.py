18ac80596ffb36272eafbfbf63996759db1145fd
alxfed <alxfed@gmail.com>
2020-03-29 09:34:37 -0500

    initial

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
 """pylez
 """
-import os
-from setuptools import setup
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-setup(
-    name = "pylez",
-    version = "0.0.1",
-    author = "Alex Fedotov",
-    author_email = "alex.fedotov@aol.com",
-    description = ("Rules Engine"),
-    license = "MIT",
-    keywords = "rules forward backward chaining",
-    url = "https://github.com/alxfed/pylez",
-    packages=['pylez', 'tests'],
-    long_description=read('README'),
+setuptools.setup(
+    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Topic :: Utilities",
+        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
